6c61f5fa85ca798bfeb308256174c98527faec1c
William Fogg <wfogg1604@gmail.com>
2022-12-22 12:21:10 +1000

    Added mat_ops.rs

--- src/mat_ops.rs
+++ src/mat_ops.rs
@@ -1,11 +1,6 @@
 extern crate nalgebra as na;
-use na::{base::allocator::Allocator, DefaultAllocator, OMatrix, Dim, Matrix3};
+use na::SMatrix;
 
-pub fn concat_horiz<T, R, C>(a: OMatrix<T, R, C>, b: OMatrix<T, R, C>) -> OMatrix<T, R, C>
-    where
-        R: Dim,
-        C: Dim,
-        DefaultAllocator: Allocator<T, R, C> {
-    let (arows, acols) = a.shape_generic();
-    return Matrix3::from_diagonal(1, 2, 3);
+pub fn concat_horiz<T, const R: usize, const C1: usize, const C2: usize, const C3: usize>(a: SMatrix<T, R, C1>, b: SMatrix<T, R, C2>) -> SMatrix<T, R, C3> {
+    return SMatrix::from_columns();
 }
