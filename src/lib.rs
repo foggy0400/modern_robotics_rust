d61170de1cbbb773518dacb1dc9010adeac7588c
William Fogg <wfogg1604@gmail.com>
2023-11-03 23:32:48 +1000

    ad function working

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[macro_use]
+pub mod concat;
 pub mod conversions;
 pub mod mat_ops;
-
--- src/mat_ops.rs
+++ src/mat_ops.rs
@@ -1,6 +1,3 @@
-extern crate nalgebra as na;
-use na::SMatrix;
-
-pub fn concat_horiz<T, const R: usize, const C1: usize, const C2: usize, const C3: usize>(a: SMatrix<T, R, C1>, b: SMatrix<T, R, C2>) -> SMatrix<T, R, C3> {
-    return SMatrix::from_columns();
-}
+// extern crate nalgebra as na;
+// use na::SMatrix;
+// use na::Vector3;
