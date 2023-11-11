2a7f33c6331f758aa08d87d232a2d5f0ef795d9c
William Fogg <wfogg1604@gmail.com>
2023-11-11 16:58:18 +1000

    added groups file

---
+++ src/groups.rs
@@ -0,0 +1,12 @@
+// File to contain definitions of different matrix types based on group theory
+// Intention is to simplify function signatures of other functions that use these matrix groups
+// Idea is to use traits to make constructing objects type-generic
+//
+// Comments are to lay out which ones to do
+//
+// SO3 matrix
+// SE3 matrix
+// Generic 3-vector
+// v (angular velocity vector) - if needing special traits
+// V (6-vector spatial velocity vec)
+// ad matrix
